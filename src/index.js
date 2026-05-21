8c486c4e765cd1fa62b4af490a3890754b189345
Rohit sonaere <69306079+Rohitsonare@users.noreply.github.com>
2026-05-21 12:01:09 +0530

    add analytics lib

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <CartProvider>
       <App />
+      <Analytics />
     </CartProvider>
   </React.StrictMode>
 );
